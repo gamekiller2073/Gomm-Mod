73c1aefd8e7b2267580105c91e320694f37f0488
gamekiller2073 <guus.rotte@gmail.com>
2025-03-06 17:38:17 +0100

    Initial commit

---
+++ src/main/java/net/guus/gommmod/GommMod.java
@@ -0,0 +1,16 @@
+package net.guus.gommmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GommMod implements ModInitializer {
+	public static final String MOD_ID = "gommmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
