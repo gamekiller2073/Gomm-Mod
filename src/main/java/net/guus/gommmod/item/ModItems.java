b4c708dfc2af15f797699874ec544f35bd3bf4be
gamekiller2073 <guus.rotte@gmail.com>
2025-03-07 19:23:31 +0100

    Made a bat

--- src/main/java/net/guus/gommmod/item/ModItems.java
+++ src/main/java/net/guus/gommmod/item/ModItems.java
@@ -1,11 +1,18 @@
 package net.guus.gommmod.item;
 
 import net.guus.gommmod.GommMod;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class ModItems {
+    public static final Item BAT = registerItem("bat", new Item(new Item.Settings()));
 
 
-
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(GommMod.MOD_ID, name), item);
+    }
 
     public static void registerModItems() {
         GommMod.LOGGER.info("Registering Mod Items for " + GommMod.MOD_ID);
